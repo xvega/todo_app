80e43f9db5ecbc4a78a233df2dd5e2a888591f49
xabi1309 <xabi1309@gmail.com>
2019-12-12 22:08:55 -0600

    Setup test env with gems and spec helper

--- Gemfile
+++ Gemfile
@@ -3,4 +3,10 @@ source 'https://rubygems.org'
 gem 'activerecord'
 gem 'standalone_migrations', '~> 5.2', '>= 5.2.7'
 gem 'sqlite3'
-gem 'rspec'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'rspec'
+  gem 'database_cleaner'
+  gem 'factory_bot'
+end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,1 +1,25 @@
-require 'yaml'
+require 'factory_bot'
+require 'active_record'
+require 'database_cleaner'
+
+RSpec.configure do |config|
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+  config.before(:each, :js => true) { DatabaseCleaner.strategy = :truncation }
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
+
+  FactoryBot.find_definitions
+
+end
+
+
+def db_configuration
+  db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
+  YAML.load(File.read(db_configuration_file))
+end
+
+ActiveRecord::Base.establish_connection(db_configuration["test"])
